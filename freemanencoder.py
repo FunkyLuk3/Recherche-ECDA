8123ba5c11929a909e41ee11b63421dd5fd7ee72
Funky Luk3 <luc.enee@gmail.com>
2022-11-29 17:45:10 +0100

    Freeman Encoder

--- freemanencoder.py
+++ freemanencoder.py
@@ -2,8 +2,6 @@ from skimage import measure
 from copy import deepcopy, copy
 
 import numpy as np
-import math
-import cv2
 
 def imgtoarray(imagename):
     data = []
@@ -15,3 +13,24 @@ def imgtoarray(imagename):
             data[nbrligne].append(ligne[pixel][1])
         nbrligne += 1
     return data
+
+def voisin(tabimage, x, y):
+    v1 = tabimage[x-1][y-1]
+    v2 = tabimage[x][y-1]
+    v3 = tabimage[x+1][y-1]
+    v4 = tabimage[x+1][y]
+    v5 = tabimage[x+1][y+1]
+    v6 = tabimage[x][y+1]
+    v7 = tabimage[x-1][y]
+    
+    return [v1,v2,v3,v4,v5,v6,v7]
+
+def freeman(tabimage):
+    code = ''
+    for ligne in range(len(tabimage)):
+        for pixel in range(len(ligne)):
+            if tabimage[ligne][pixel] != 0:
+                listvoisin = voisin(tabimage, ligne, pixel)
+                for i in range(listvoisin):
+                    if voisin[i] != 0:
+                        code += str(i+1)
--- main.py
+++ main.py
@@ -5,5 +5,6 @@ def main():
     skel = skeletonizer('bdd/04_2PS600_police12/00_resize/a/001.png')
     data = imgtoarray(skel)
     affichage('Skeletonize', skel)
+    print(data)
 
 main()

51191e375db645aa9e3f0dcec572fc76d4423371
Evan L'HUISSIER <e.lhuissier@protonmail.com>
2022-11-30 10:22:10 +0100

    changé ordre dans le main

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from skeletonize import affichage, skeletonizer
 from freemanencoder import *
 
 def main():
-    skel = skeletonizer('bdd/04_2PS600_police12/00_resize/a/003.png')
+    skel = skeletonizer('bdd/01_Numeric_police12/00_resize/e/001.png')
     data = imgtoarray(skel)
-    affichage('Skeletonize', skel)
     print(freeman(data))
+    affichage('Skeletonize', skel)
 
 main()
